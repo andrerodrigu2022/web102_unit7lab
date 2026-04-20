20057c04ddebfc53db511efef307fc1c437d818b
Andre Rodriguez <andrerodrigu2022@fau.edu>
2026-04-19 22:52:22 -0400

    Added delete functionn

--- src/pages/EditPost.jsx
+++ src/pages/EditPost.jsx
@@ -29,6 +29,17 @@ const EditPost = ({data}) => {
         window.location = "/";
     }
 
+    const deletePost = async (event) => {
+        event.preventDefault();
+
+        await supabase
+            .from('Posts')
+            .delete()
+            .eq('id', id);
+
+        window.location = "/";
+    }
+
     return (
         <div>
             <form>
@@ -45,7 +56,7 @@ const EditPost = ({data}) => {
                 </textarea>
                 <br/>
                 <input type="submit" value="Submit" onClick={updatePost} />
-                <button className="deleteButton">Delete</button>
+                <button className="deleteButton" onClick={deletePost}>Delete</button>
             </form>
         </div>
     )
